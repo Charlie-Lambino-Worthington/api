9c3bdf31a4c22125b2239d08d5082760dd2216d1
Charlie Lambino-Worthington <charlie.m.k.worthington@gmail.com>
2024-05-21 09:51:23 +0000

    fix error

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,35 +17,15 @@ async function postForm(e) {
         body: form,
     });
 
-}
+    const data = await response.json();
 
-const data = await response.json();
-
-if (response.ok) {
-    displayErrors(data);
-} else {
-    throw new Error(data.error);
-}
-
-function displayErrors(data) {
-    let heading = `JSHint Results for ${data.file}`;
-    if (data.total_errors === 0) {
-        results = `<div class="no_errors">No errors reported!</div>`;
+    if (response.ok) {
+        displayErrors(data);
     } else {
-        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`;
-        for (let error of data.error_list) {
-            results += `<div>At line <span class="line">${error.line}</span>, `;
-            results += `column <span class="column">${error.col}:</span></div>`;
-            results += `<div class="error">${error.error}</div>`;
-        }
-
-
+        throw new Error(data.error);
     }
-    document.getElementById("resultsModalTitle").innerText = heading;
-    document.getElementById("results-content").innerHTML = results;
-    resultsModal.show();
-}
 
+}
 
 async function getStatus(e) {
 
@@ -56,20 +36,42 @@ async function getStatus(e) {
     const data = await response.json();
 
     if (response.ok) {
-        displayStatus(data.expiry);
+        displayStatus(data);
     } else {
         throw new Error(data.error);
     }
 
 }
 
-function displayStaus(data) {
-    let heading = "API KEY STATUS";
+function displayErrors(data) {
+
+    let results = "";
+
+    let heading = `JSHint Results for ${data.file}`;
+    if (data.total_errors === 0) {
+        results = `<div class="no_errors">No errors reported!</div>`;
+    } else {
+        results = `<div>Total Errors: <span class="error_count">${data.total_errors}</span></div>`;
+        for (let error of data.error_list) {
+            results += `<div>At line <span class="line">${error.line}</span>, `;
+            results += `column <span class="column">${error.col}:</span></div>`;
+            results += `<div class="error">${error.error}</div>`;
+        }
+    }
+
+    document.getElementById("resultsModalTitle").innerText = heading;
+    document.getElementById("results-content").innerHTML = results;
+    resultsModal.show();
+}
+
+function displayStatus(data) {
+
+    let heading = "API Key Status";
     let results = `<div>Your key is valid until</div>`;
     results += `<div class="key-status">${data.expiry}</div>`;
 
     document.getElementById("resultsModalTitle").innerText = heading;
-    document.getElementById("results-content").innerText = results;
-    resultsModal.show()
-}
+    document.getElementById("results-content").innerHTML = results;S
+    resultsModal.show();
 
+}
